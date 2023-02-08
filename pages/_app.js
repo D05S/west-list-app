070cedf7c56579e22697f0f194b5c46c7ed56136
D05S <dominiksauer76@gmail.com>
2023-02-08 19:39:33 +0100

    cover to the notch

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
         <Head>
-           <meta name="viewport" content="width=device-width, initial-scale=1" />
+           <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" /> 
            <link rel="icon" type="image/png" href="/img/option.png" />
            <title>The West List</title>
         </Head>
